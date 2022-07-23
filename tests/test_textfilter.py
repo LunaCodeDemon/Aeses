6abac26be0199602809c3dd5117529a1190b8f48
LunaLilDragon <luna@aciralith.com>
2022-07-23 19:25:40 +0200

    Added pytest and tests for textfilter.py
    Added a few unit tests.

---
+++ tests/test_textfilter.py
@@ -0,0 +1,13 @@
+"Testing functions of textfilter.py"
+
+from scripts.textfilter import check_for_emoji, check_for_links
+
+def test_emoji_check():
+    "Test if emoji can be filtered."
+    result = check_for_emoji("acdäü❤️-2")
+    assert result is True
+
+def test_link_check():
+    "Test if links can be detected."
+    result = check_for_links("Click here now: https://youtube.com/?w=ohmyfish")
+    assert result is True
