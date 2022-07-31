af8aed1968f164e9777e66c4c9bbeac8b9e51efd
LunaLilDragon <luna@aciralith.com>
2022-07-31 22:14:35 +0200

    log channel can now be changed.

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,5 +1,8 @@
 "This Cog lets server moderators change settings for the guild."
+from discord import TextChannel, Permissions
 from discord.ext import commands
+
+from scripts.sqldata import insert_logchannel
 
 class Settings(commands.Cog):
     "Moderative group to set guild settings."
@@ -7,11 +10,17 @@ class Settings(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def logchannel(self, ctx: commands.Context):
-        "Sets a logging channel. (WIP)"
-        await ctx.send("This command is work in progress.")
-
-        # TODO: set log channel in database
+    async def logchannel(self, ctx: commands.Context, *, log_channel: TextChannel):
+        "Sets a logging channel."
+        if log_channel:
+            perms: Permissions = ctx.author.permissions_for(self)
+            if perms.administrator:
+                insert_logchannel(ctx.guild.id, log_channel.id)
+                await ctx.send(f"Set {log_channel.mention} as logging channel.")
+            else:
+                await ctx.send("You do not have the correct permissions to do that.")
+        else:
+            await ctx.send_help("logchannel")
 
 def setup(client: commands.Bot):
     "Setup function for settings COG"
---
+++ scripts/sqldata.py
@@ -0,0 +1,49 @@
+"Handles most sql actions"
+from sqlalchemy import create_engine, text
+
+engine = create_engine("sqlite:///:memory:", echo=True)
+
+def create_table_logchannel():
+    "Create a table for log channel"
+    with engine.connect() as conn:
+        conn.execute(
+            text("CREATE TABLE IF NOT EXISTS logchannels"\
+                "(channel_id unsigned long, "\
+                "guild_id unsigned long);"))
+
+
+def get_logchannel(guild_id: int) -> int | None:
+    "Get the log channel of the guild."
+    channel_id: int | None = None
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT channel_id FROM logchannels "\
+                "WHERE guild_id == :guild_id"),
+                [{"guild_id": guild_id}]
+        ).first()
+        channel_id = result[0] if result else None
+    return channel_id
+
+def update_logchannel(guild_id: int, channel_id: int) -> None:
+    "Update channel_id for logchannel in guild."
+    with engine.connect() as conn:
+        conn.execute(
+            text("UPDATE logchannels"\
+                "SET channel_id = :channel_id"\
+                "WHERE guild_id = :guild_id"),
+            [{"guild_id": guild_id, "channel_id": channel_id}]
+        )
+
+def insert_logchannel(guild_id: int, channel_id: int) -> None:
+    "Insert logchannel into database"
+
+    # Guard to prevent having multiple log channels.
+    if get_logchannel(guild_id):
+        update_logchannel(guild_id, channel_id)
+        return
+
+    with engine.connect() as conn:
+        conn.execute(
+            text("INSERT INTO logchannels (guild_id, channel_id)"\
+                " VALUES (:guild_id, :channel_id)"),
+            [{"guild_id": int(guild_id), "channel_id": int(channel_id)}])
