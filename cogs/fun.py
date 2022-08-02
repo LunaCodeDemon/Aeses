4813d0cbe820a3e2491ab14870410d0ae559fc55
LunaLilDragon <luna@aciralith.com>
2022-08-02 18:19:27 +0200

    Added pokemon command.

---
+++ cogs/fun.py
@@ -0,0 +1,29 @@
+"Module for fun commands."
+from discord.ext import commands
+from scripts import pokeapi
+
+class Fun(commands.Cog):
+    "This cog contains various fun commands"
+    def __init__(self, client: commands.Bot):
+        "Initialiser for 'Fun' cog."
+        self.client = client
+
+    @commands.command()
+    async def pokemon(self, ctx: commands.Context, *, name: str = None):
+        "Searches for a pokemon."
+        # grab data from pokeapi
+        pokemon_data: any
+        if name:
+            pokemon_data = pokeapi.get_pokemon(name)
+        else:
+            pokemon_data = pokeapi.get_random_pokemon()
+        # turn data into embed
+        pokemon_embed = pokeapi.gen_pokemon_embed(pokemon_data)
+        if not pokemon_embed:
+            await ctx.send_help('pokemon') # failed
+            return
+        await ctx.send(embed=pokemon_embed)
+
+def setup(client: commands.Bot):
+    "Setup function for Fun command collection."
+    client.add_cog(Fun(client))
---
+++ scripts/pokeapi.py
@@ -0,0 +1,49 @@
+"This module interfaces with pokeapi"
+from functools import cache, lru_cache
+from random import choice
+import httpx
+from discord import Embed
+
+POKEAPI_BASEURL = "https://pokeapi.co/api/v2"
+
+@cache
+def get_full_pokemon_list():
+    "Get the entire list of pokemon that exist."
+    return httpx.get(f"{POKEAPI_BASEURL}/pokemon?limit=100000&offset=0").json()
+
+@lru_cache(maxsize=50)
+def get_pokemon(search_tag: str):
+    "Get a pokemon from pokeapi."
+    return httpx.get(f"{POKEAPI_BASEURL}/pokemon/{search_tag}").json()
+
+def get_random_pokemon():
+    "Get a random pokemon from pokeapi"
+    name = choice(get_full_pokemon_list()['results'])['name']
+    return get_pokemon(name)
+
+def seek_optimial_front_sprite(data) -> str:
+    "Seek for default front sprites for a pokemon."
+    other = data['sprites']['other']
+    img_link = data['sprites']['front_default']
+    sprite_list = [other[v] for v in other if v in ["home", "official-artwork"]]
+
+    # seek if better one exists.
+    for sprite in sprite_list:
+        if sprite['front_default']:
+            img_link = sprite['front_default']
+            break
+    return img_link
+
+def gen_pokemon_embed(data) -> Embed:
+    "Generate an embed for given pokemon."
+    if not data:
+        raise ValueError
+
+    img_link = seek_optimial_front_sprite(data)
+    ptypes = [t['type']['name'] for t in data['types']]
+
+    embed = Embed()
+    embed.title = data['name']
+    embed.add_field(name="Types", value=','.join(ptypes))
+    embed.set_image(url=img_link)
+    return embed
