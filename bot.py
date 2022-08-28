e02319779b54a04c6c4debea80b1678d3d80e900
ChinoCodeDemon <luna@aciralith.com>
2022-08-28 14:05:21 +0000

    changed loose listeners to be a part of bot class.

--- bot.py
+++ bot.py
@@ -67,64 +67,53 @@ class AesesBot(commands.Bot):
         for menu in menus:
             self.tree.add_command(menu)
 
+    # pylint: disable=arguments-differ
+    async def on_message(self, message: discord.Message):
+        "This will be triggered whenever a user sends a message."
+        if message.author == client.user:
+            return
 
-client = AesesBot(command_prefix="!", intents=discord.Intents.all())
+        # filter
+        if await check_message(message):
+            return
 
+        await client.process_commands(message)
 
-@client.event
-async def on_message(message: discord.Message):
-    "This will be triggered whenever a user sends a message."
-    if message.author == client.user:
-        return
+    async def on_message_edit(self, _: discord.Message, updated: discord.Message):
+        "This will be triggered whenever a user edits a message."
+        if updated.author == client.user:
+            return
 
-    # filter
-    if await check_message(message):
-        return
+        # filter
+        if await check_message(updated):
+            return
 
-    await client.process_commands(message)
+    async def on_member_update(self, _: discord.Member, after: discord.Member):
+        "React on updates of the member"
+        client_member: discord.Member = await after.guild.fetch_member(client.user.id)
+        if client_member.guild_permissions.manage_nicknames and check_nickname(after):
+            try:
+                if after.nick is None:
+                    await after.edit(nick=str2only_ascii(after.display_name))
+                else:
+                    await after.edit(nick=None)
+            except (discord.Forbidden, HTTPException) as err:
+                logging.exception(err)
 
+    async def on_command_error(self, ctx: commands.Context, error: BaseException):
+        "Handles errors for every command."
+        if isinstance(error, commands.CommandInvokeError):
+            error = error.original
 
-@client.event
-async def on_message_edit(_: discord.Message, updated: discord.Message):
-    "This will be triggered whenever a user edits a message."
-    if updated.author == client.user:
-        return
+        exc_func = error_dictionary.get(error)
+        if exc_func:
+            exc_func(ctx, error)
+        else:
+            logging.exception(error)
+            raise error
 
-    # filter
-    if await check_message(updated):
-        return
-
-
-@client.event
-async def on_member_update(_: discord.Member, after: discord.Member):
-    "React on updates of the member"
-    client_member: discord.Member = await after.guild.fetch_member(client.user.id)
-    if client_member.guild_permissions.manage_nicknames and check_nickname(after):
-        try:
-            if after.nick is None:
-                await after.edit(nick=str2only_ascii(after.display_name))
-            else:
-                await after.edit(nick=None)
-        except (discord.Forbidden, HTTPException) as err:
-            logging.exception(err)
-
-
-@client.event
-async def on_command_error(ctx: commands.Context, error: BaseException):
-    "Handles errors for every command."
-    if isinstance(error, commands.CommandInvokeError):
-        error = error.original
-
-    exc_func = error_dictionary.get(error)
-    if exc_func:
-        exc_func(ctx, error)
-    else:
-        logging.exception(error)
-        raise error
+    async def on_error(self, event_method: str, /, *args) -> None:
+        logging.error(event_method, args)
 
 
-@client.event
-async def on_error(error: BaseException):
-    "log errors"
-    logging.exception(error)
-    raise error
+client = AesesBot(command_prefix=DEFAULT_PREFIX, intents=discord.Intents.all())
