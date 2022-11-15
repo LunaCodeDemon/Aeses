83f591e63d962b92cf2967a8cff9ab33a495d451
Luna Köpke <luna@aciralith.com>
2022-11-15 22:34:13 +0100

    Rename config.yml (#11)
    * renamed config.yml to dialogs.yml
    
    * correct typo in loader l8.

--- configloader.py
+++ configloader.py
@@ -2,7 +2,7 @@
 import yaml
 
 # pylint: disable=consider-using-with
-config = yaml.full_load(open("config.yml", encoding="utf-8"))
+config = yaml.full_load(open("dialogs.yml", encoding="utf-8"))
 
 # pylint: disable=consider-using-with
 emote_links = yaml.full_load(open("emote-links.yml", encoding="utf-8"))
