22e34b00dcf7c035170e511879274cf7cd3eede8
Luna Köpke <luna@aciralith.com>
2023-02-06 19:52:02 +0100

    removing unecessary class

--- scripts/name_randomizer.py
+++ scripts/name_randomizer.py
@@ -6,41 +6,22 @@ from typing import List
 import random
 import functools
 
-
-class NameRandomizer:
-    """
-        A class to randomly pick names.
-    """
-    names: List[str]
-
-    def __init__(self) -> None:
-        """
-            Class initializer will fill name list from a file.
-        """
-        with open('forenames.txt', encoding='utf-8') as file:
-            self.names = file.readlines()
-
-    def pick(self):
-        """
-            A function that randomly picks a name from the name list.
-        """
-        return random.choice(self.names)
-
 def wrap_name_randomizer(func):
     """
-        Wrapper that creates name randomizer object for a certain function.
+        Wrapper that loads names from file for a certain function.
     """
-    nrand = NameRandomizer()
+    with open('forenames.txt', encoding='utf-8') as file:
+        names = file.readlines()
 
     @functools.wraps(func)
     def inner():
-        return pick_randomized_name(nrand)
+        return pick_randomized_name(names)
 
     return inner
 
 @wrap_name_randomizer
-def pick_randomized_name(nrand: NameRandomizer):
+def pick_randomized_name(names: List[str]):
     """
-        Picks a random name via a name picker class.
+        A function that randomly picks a name from the name list.
     """
-    return nrand.pick()
+    return random.choice(names)
