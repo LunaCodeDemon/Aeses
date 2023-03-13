7735ea868c1a05d3c17e78a689b69503ec1fb7a8
Luna Köpke <luna@aciralith.com>
2023-03-13 23:59:46 +0100

    Formatte radio_browser.py

--- api/radio_browser.py
+++ api/radio_browser.py
@@ -2,11 +2,14 @@
     API Methods for radio-browser
 """
 import socket
-import httpx
 from typing import List, NamedTuple
-from random import choice
+import httpx
+
 
 class RadioEntry(NamedTuple):
+    """
+        Commonly used data that a station has.
+    """
     stationuuid: str
     name: str
     url: str
@@ -17,16 +20,16 @@ class RadioEntry(NamedTuple):
     languagecodes: str
     votes: int
 
+
 def get_ip_for_radio_browser() -> List[str]:
     """
         Gives all the important addresses of radio browser.
         (Documentation recommends calling the API via IP)
     """
     return [
-        info[4][0]
-        for info in
-        socket.getaddrinfo("all.api.radio-browser.info")
-        ]
+        info[4][0] for info in socket.getaddrinfo("all.api.radio-browser.info")
+    ]
+
 
 def search_radio(name: str) -> List[RadioEntry]:
     """
@@ -35,24 +38,25 @@ def search_radio(name: str) -> List[RadioEntry]:
     # TODO: use ip instead of general domain.
     #api_ip = choice(get_ip_for_radio_browser())
     #print(api_ip)
-    result = httpx.get("http://all.api.radio-browser.info/json/stations/search",
-            params={
+    result = httpx.get(
+        "http://all.api.radio-browser.info/json/stations/search",
+        params={
             "name": name,
             "limit": 10,
             "offset": 0
-            })
+        })
 
     if result.status_code != 200:
         return []
-    
-    return [RadioEntry(
-        stationuuid=raw_entry["stationuuid"],
-        name=raw_entry["name"],
-        url=raw_entry["url"],
-        url_resolved=raw_entry["url_resolved"],
-        homepage=raw_entry["homepage"],
-        tags=raw_entry["tags"],
-        language=raw_entry["language"],
-        languagecodes=raw_entry["languagecodes"],
-        votes=raw_entry["votes"]
-    ) for raw_entry in result.json()]
+
+    return [
+        RadioEntry(stationuuid=raw_entry["stationuuid"],
+                   name=raw_entry["name"],
+                   url=raw_entry["url"],
+                   url_resolved=raw_entry["url_resolved"],
+                   homepage=raw_entry["homepage"],
+                   tags=raw_entry["tags"],
+                   language=raw_entry["language"],
+                   languagecodes=raw_entry["languagecodes"],
+                   votes=raw_entry["votes"]) for raw_entry in result.json()
+    ]
